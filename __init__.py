3de42efd5162ce16db4221d5aafdce2d797148a6
Yura-Hameliak <yurka6677@gmail.com>
2024-03-09 19:53:28 +0200

    file arhitecture changes

--- __init__.py
+++ __init__.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/songs")
 def index():
     return render_template("songs.html")
+
+@app.route("/playlists")
+def playlists():
+    return render_template("playlists.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
