b61f7379174bfbbd0dd44c317bb930c89368d400
cheeese <tepliakov.pn@ucu.edu.ua>
2024-03-10 17:26:57 +0200

    added flask
    changed structure of files for flask

---
+++ __init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+uri = "mongodb+srv://yurora:vervor67vervor@cluster0.pkxylky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri)
+db = client['Tempotune']
+users = db["users"]
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
