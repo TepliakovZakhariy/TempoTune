fed7e3eb0093143def3c767a6d5522789a7423e5
Yura-Hameliak <yurka6677@gmail.com>
2024-03-10 19:56:00 +0200

    added starter flask for logi

--- __init__.py
+++ __init__.py
@@ -5,19 +5,34 @@ from bson.objectid import ObjectId
 uri = "mongodb+srv://yurora:vervor67vervor@cluster0.pkxylky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(uri)
-db = client['Tempotune']
+db = client['TempoTune']
 users = db["users"]
 app = Flask(__name__)
 
-
+# class User:
+#     """a"""
+#     def __init__(self,u_id,name,email,password) -> None:
+#         self.id = u_id
+#         self.name=name
+#         self.email=email
+#         self.password=password
+#         playlists = []
+#     def __str__(self) -> str:
+#         return f'{self.name} : '
+# class 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        username=request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        users.insert_one({'username': username, 'email': email, 'password': password})
+        return redirect(url_for('index'))
     return render_template('login.html')
-
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
