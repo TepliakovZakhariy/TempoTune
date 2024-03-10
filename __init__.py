5ce2e5f9c42a986a3759dc7692df53791ea6f388
Yura-Hameliak <yurka6677@gmail.com>
2024-03-10 18:11:39 +0200

    few files added and hrefs changed

--- __init__.py
+++ __init__.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for,redirect,request
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+uri = "mongodb+srv://yurora:vervor67vervor@cluster0.pkxylky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri)
+db = client['Tempotune']
+users = db["users"]
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/songs")
-def index():
+def songs():
     return render_template("songs.html")
 
 @app.route("/playlists")
 def playlists():
     return render_template("playlists.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
