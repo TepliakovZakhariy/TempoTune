55b2821bfbc1d29e6ec3b44a2122e822f7f43829
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-03-18 16:54:08 +0200

    changes in app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from random import getrandbits
